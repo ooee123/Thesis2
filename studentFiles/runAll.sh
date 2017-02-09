44539cc9ec3d6038a3594ee873975b48d032f31c
Kevin Ly <ooee123@gmail.com>
2017-02-09 10:28:47 -0800

    Making graphs

--- studentFiles/runAll.sh
+++ studentFiles/runAll.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for D in *; do
-   if [ -d "${D}" ]; then
+   if [ -d "${D}" ] && [ "${D}" != "graphs" ]; then
       cd $D
       $@
       cd ..
--- studentFiles/summaryPlot.py
+++ studentFiles/summaryPlot.py
@@ -2,8 +2,12 @@
 
 import os
 import json
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy
+import shutil
+
 
 aggregate = {"original": {"Simian": {"clones": 0, "lines": 0}, "JPlag": {"clones": 0, "lines": 0}, "CloneDR": {"clones": 0, "lines": 0}}, "mine_noSort": {"Simian": {"clones": 0, "lines": 0}, "JPlag": {"clones": 0, "lines": 0}, "CloneDR": {"clones": 0, "lines": 0}}, "mine_noID": {"Simian": {"clones": 0, "lines": 0}, "JPlag": {"clones": 0, "lines": 0}, "CloneDR": {"clones": 0, "lines": 0}}, "mine": {"Simian": {"clones": 0, "lines": 0}, "JPlag": {"clones": 0, "lines": 0}, "Moss": {"clones": 0, "lines": 0}, "CloneDR": {"clones": 0, "lines": 0}}, "mine_nothing": {"Simian": {"clones": 0, "lines": 0}, "JPlag": {"clones": 0, "lines": 0}, "Moss": {"clones": 0, "lines": 0}, "CloneDR": {"clones": 0, "lines": 0}}, "mine_noID_moss": {"Simian": {"clones": 0, "lines": 0}, "JPlag": {"clones": 0, "lines": 0}, "CloneDR": {"clones": 0, "lines": 0}}, "mine_moss": {"Simian": {"clones": 0, "lines": 0}, "JPlag": {"clones": 0, "lines": 0}, "CloneDR": {"clones": 0, "lines": 0}}}
 
@@ -13,6 +17,10 @@ difference = {}
 def main():
    global students
    global difference
+   graphsDirectory = 'graphs'
+   if os.path.isdir(graphsDirectory):
+      shutil.rmtree(graphsDirectory)
+   os.mkdir(graphsDirectory)
    files = os.listdir(".")
    for f in files:
       if os.path.isdir(f):
@@ -22,8 +30,10 @@ def main():
             results = json.load(summary)
             students[f] = results
             print(f)
-            beforeAfterBars(results, "JPlag")
-            beforeAfterBars(results, "Simian")
+            beforeAfterBars(results, "JPlag", f, graphsDirectory)
+            beforeAfterBars(results, "Simian", f, graphsDirectory)
+            beforeAfterBars(results, "CloneDR", f, graphsDirectory)
+            beforeAfterBars(results, "Moss", f, graphsDirectory)
             for key in results:
                for tool in results[key]:
                   for data in results[key][tool]:
@@ -32,7 +42,10 @@ def main():
          print("Summary file not found for " + f)
    aggregateDifferences = processDifferences(aggregate)
    print(json.dumps(aggregate, indent = 4))
-   beforeAfterBars(aggregate, "JPlag")
+   beforeAfterBars(aggregate, "JPlag", "summary", graphsDirectory)
+   beforeAfterBars(aggregate, "Simian", "summary", graphsDirectory)
+   beforeAfterBars(aggregate, "CloneDR", "summary", graphsDirectory)
+   beforeAfterBars(aggregate, "Moss", "summary", graphsDirectory)
    for student in students:
       difference[student] = processDifferences(students[student])
 
@@ -53,17 +66,23 @@ def processToolDifferent(before, after):
             difference[tool][data] = after[tool][data] - before[tool][data]
    return difference
 
-def beforeAfterBars(result, toolName):
-   ind = range(len(result.keys()))
+def beforeAfterBars(result, toolName, studentName, filePath="."):
    plt.title(toolName)
    width = 0.35
    barNumber = 0
+   bars = 0
    for version in result:
-      height = result[version][toolName]["lines"]
-      original = plt.bar(barNumber, height, width = width, align='center')
-      plt.text(barNumber, height, str(height), ha='center', va='bottom')
-      barNumber += 1
+      if toolName in result[version]:
+         bars += 1
+         height = result[version][toolName]["lines"]
+         original = plt.bar(barNumber, height, width = width, align='center')
+         plt.text(barNumber, height, str(height), ha='center', va='bottom')
+         barNumber += 1
+      else:
+         pass
+   ind = range(bars)
    plt.xticks(ind, result.keys())
-   plt.show()
+   plt.savefig(os.path.join(filePath, studentName + "_" + toolName), bbox_inches='tight')
+   plt.clf()
 
 main()
